10a4b29dadb56d5297c4bce82ef2dd6d63f22605
cynde <cynthiacyndewi@gmail.com>
2024-03-16 13:19:29 +0800

    add login to check whether current player win

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const initialPlayer = 'o';
+  const boardSize = 3;
+  const initialPlayer = 'O';
   const initialBoard = [
     ['+', '+', '+'],
     ['+', '+', '+'],
@@ -18,18 +19,79 @@ function App() {
   const [board, setBoard] = useState(initialBoard);
   const [isDisabled, setIsDisabled] = useState(initialIsDisabled);
 
-  const [count, setCount] = useState(0);
+  const [moveCount, setMoveCount] = useState(0);
   const [xWinCount, setXWinCount] = useState(0);
   const [oWinCount, setOWinCount] = useState(0);
+
+  const [winner, setWinner] = useState(null);
+  const [isTie, setIsTie] = useState(false);
+
+  const isCurrentPlayerWin = (selectedXIndex, selectedYIndex) => {
+    for (let yIndex = 0; yIndex < boardSize; yIndex++) {
+      if (board[selectedXIndex][yIndex] !== player) {
+        break;
+      }
+      if (yIndex === boardSize - 1) {
+        return true;
+      }
+    }
+
+    for (let xIndex = 0; xIndex < boardSize; xIndex++) {
+      if (board[xIndex][selectedYIndex] !== player) {
+        break;
+      }
+      if (xIndex === boardSize - 1) {
+        return true;
+      }
+    }
+
+    if (selectedXIndex === selectedYIndex) {
+      for (let index = 0; index < boardSize; index++) {
+        if (board[index][index] !== player) {
+          break;
+        }
+        if (index === boardSize - 1) {
+          return true;
+        }
+      }
+    }
+
+    if (selectedXIndex + selectedYIndex === boardSize - 1) {
+      for (let index = 0; index < boardSize; index++) {
+        if (board[index][(boardSize - 1) - index] !== player) {
+          break;
+        }
+        if (index === boardSize - 1) {
+          return true;
+        }
+      }
+    }
+
+    if (moveCount === (Math.pow(boardSize, 2) - 1)) {
+      setIsTie(true);
+    }
+  };
 
   const handleRestart = () => {
     setPlayer(initialPlayer);
     setBoard(initialBoard);
     setIsDisabled(initialIsDisabled);
-    setCount(0);
+    setMoveCount(0);
+    setWinner(null);
+    setIsTie(false);
   };
 
   const handleCellClick = (xIndex, yIndex) => {
+    if (winner) {
+      alert(`${winner.toUpperCase()} has won the game. Start a new game`);
+      return handleRestart();
+    }
+
+    if (isTie) {
+      alert('Its a tie. It will restart.');
+      return handleRestart();
+    }
+
     if (isDisabled[xIndex][yIndex]) {
       return alert('Already selected');
     }
@@ -40,15 +102,20 @@ function App() {
     isDisabled[xIndex][yIndex] = true;
     setIsDisabled(isDisabled);
 
-    const newPlayer = player === 'o' ? 'x' : 'o';
+    const newPlayer = player === 'O' ? 'X' : 'O';
     setPlayer(newPlayer);
 
-    setCount(count + 1);
+    setMoveCount(moveCount + 1);
+
+    if (isCurrentPlayerWin(xIndex, yIndex)) {
+      setWinner(player);
+      alert(`${player.toUpperCase()} wins`);
+    };
   };
 
   const getCellClassName = (cell) => {
     return (
-      `btn span1${cell === 'o' ? ' btn-primary' : cell === 'x' ? ' btn-info' : ''}`
+      `btn span1${cell === 'O' ? ' btn-primary' : cell === 'X' ? ' btn-info' : ''}`
     );
   }
 
